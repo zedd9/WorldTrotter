bd167461e84af39933619fb47c3855158c25f645
Zedd <gusdnre@gmail.com>
2021-02-18 23:56:23 +0900

    아론 힐리가스의 iOS 프로그래밍 3장 : 뷰와 뷰 계층 구조 4장 : 테스트 입력과 델리게이션 + 동메달 과제 : 알파벳 문자 허용하지 않기

---
+++ WorldTrotter/ConversionViewController.swift
@@ -0,0 +1,77 @@
+//
+//  ConversionViewController.swift
+//  WorldTrotter
+//
+//  Created by 신현욱 on 2021/02/18.
+//
+
+import UIKit
+
+class ConversionViewController: UIViewController, UITextFieldDelegate {
+    @IBOutlet var celsiusLabel: UILabel!
+    @IBOutlet var textField: UITextField!
+    
+    let numberFormatter: NumberFormatter = {
+        let nf = NumberFormatter()
+        nf.numberStyle = .decimal
+        nf.minimumFractionDigits = 0
+        nf.maximumFractionDigits = 1
+        return nf
+    }()
+    
+    var fahrenheitValue: Double? {
+        didSet{
+            updateCelsiusLabel()
+        }
+    }
+    
+    var celsiusValue: Double? {
+        if let value = fahrenheitValue {
+            return (value - 32) * (5/9)
+        } else {
+            return nil
+        }
+    }
+    
+    func updateCelsiusLabel() {
+        if let value = celsiusValue {
+            celsiusLabel.text = numberFormatter.string(from:NSNumber(value:value))
+        } else {
+            celsiusLabel.text = "???"
+        }
+    }
+    
+    @IBAction func fahrenheitFieldEdiginChnaged(textField: UITextField){
+        if let text = textField.text, let value = Double(text){
+            fahrenheitValue = value
+        } else {
+            fahrenheitValue = nil
+        }
+    }
+    
+    @IBAction func dismissKeyboard(sender: AnyObject){
+        textField.resignFirstResponder()
+    }
+    
+    func textField(_ textField: UITextField, shouldChangeCharactersIn range: NSRange, replacementString string: String) -> Bool {
+        let currentText = textField.text ?? ""
+        let length = string.count - range.length + currentText.count
+        if length > 7 {
+            return false;
+        }
+        
+        let alphabetSet = CharacterSet(charactersIn: "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ")
+        if string.rangeOfCharacter(from: alphabetSet) != nil {
+            return false;
+        }
+        
+        let existingTextHasDecimalSeparator = currentText.rangeOfCharacter(from: CharacterSet(charactersIn: "."))
+        let repalcementTextHasDecimalSeparator = string.rangeOfCharacter(from: CharacterSet(charactersIn: "."))
+        
+        if existingTextHasDecimalSeparator != nil && repalcementTextHasDecimalSeparator != nil {
+            return false
+        } else {
+            return true;
+        }
+    }
+}
